ee724d92ee4c0e39c41fa7dd97ccdf2fa1b956de
chojsm <chojs28@gmail.com>
2022-04-06 14:33:52 +0900

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">Header</header>
+      <main>Hello</main>
+    </div>
+  );
+}
+
+export default App;
